4b45f58efa2cece20749386b58b958092a3b2c02
YotioSoft <ytani0323@gmail.com>
2019-12-07 13:00:22 +0900

    Windows版もOpenSiv3D v0.4.2にアップデート

---
+++ vs/Cities Box.cpp/Cities Box.cpp/Images.cpp
@@ -0,0 +1,45 @@
+//
+//  Images.cpp
+//  Cities Box
+//
+//  Created by YotioSoft on 2019/11/27.
+//
+
+#include "Images.hpp"
+
+void Images::load(string file_path, string tag, Color transparent) {
+	// ファイル一覧を取得
+	Array<FileStruct> image_files = specific::getAllFilesName(file_path, "png");
+	
+	// 各ファイルを読み込み
+	for (int i=0; i<image_files.size(); i++) {
+		Image image_temp(Unicode::Widen(image_files[i].file_path));
+		
+		if (transparent.r >= 0 && transparent.g >= 0 && transparent.b >= 0) {
+			for (int h=0; h<image_temp.height(); h++) {
+				for (int w=0; w<image_temp.width(); w++) {
+					if (image_temp[h][w].r == transparent.r && image_temp[h][w].g == transparent.g && image_temp[h][w].b == transparent.b) {
+						image_temp[h][w].setA(0);		// アルファ値を0に設定
+					}
+				}
+			}
+		}
+		
+		ImageStruct new_image;
+		new_image.texture = Texture(image_temp);
+		new_image.file_path = file_path;
+		new_image.file_name = image_files[i].file_path;
+		new_image.tag = tag;
+		
+		images[tag][split(image_files[i].file_name, ".")[0]] = new_image;
+	}
+}
+
+void Images::load(string file_path, string tag) {
+	Color none_clear;
+	none_clear.r = -1;
+	none_clear.g = -1;
+	none_clear.b = -1;
+	
+	load(file_path, tag, none_clear);
+}
---
+++ vs/Cities Box.cpp/Cities Box.cpp/Main.cpp
@@ -0,0 +1,123 @@
+#include "Specific.hpp"
+
+#include "StartUp.hpp"
+#include "TitleMenu.hpp"
+#include "Images.hpp"
+#include "Addon.hpp"
+#include "CityMap.hpp"
+#include "SubWindow.hpp"
+
+void Main() {
+	Window::SetTitle(U"Cities Box.cpp");
+	Scene::SetBackground(Color(50, 50, 50));
+	
+	/*---------------------------------------------
+	 				ファイル読み込み
+	 --------------------------------------------*/	
+	// 画像の読み込み
+	Images images;
+	loadImages(images);
+	
+	// フォントの宣言
+	Font font16(16);
+	
+	// タイトルメニュー画面
+	string map_file_path;
+	if (!titleMenu(images, font16, map_file_path)) {
+		return;				// タイトル画面でウィンドウを閉じたらプログラム終了
+	}
+	
+	// マップとアドオンの読み込み
+	CityMap map;
+	map.load(map_file_path);
+	
+	// カメラの初期位置
+	CameraStruct camera;
+	camera.position = PositionStruct{-Scene::Width()/2+64/2, -150};
+	
+	// カーソルの位置
+	CursorStruct cursor;
+	cursor.texture = &(images.images["pointer"]["blue"].texture);
+	
+	PositionStruct cursor_position, cursor_position_before;
+	
+	// 描画処理
+	RenderTexture buffer_texture(Scene::Width(), Scene::Height(), Color(50, 50, 50));
+	bool update_map = true, first_loop = true;
+	
+	// サブウィンドウ
+	SubWindow sub_window(U"Test Window", &font16, SizeStruct{400, 200}, Color(Palette::White));
+	SubWindow sub_window2(U"Test Window2", &font16, SizeStruct{300, 150}, Color(Palette::White));
+	
+	while (System::Update()) {
+		// カメラの操作
+		if (KeyLeft.pressed()) {
+			camera.position.x -= 20;
+			update_map = true;
+		}
+		if (KeyRight.pressed()) {
+			camera.position.x += 20;
+			update_map = true;
+		}
+		if (KeyUp.pressed()) {
+			camera.position.y -= 10;
+			update_map = true;
+		}
+		if (KeyDown.pressed()) {
+			camera.position.y += 10;
+			update_map = true;
+		}
+		
+		// カーソルの位置を取得
+		cursor_position = PositionStruct{Cursor::Pos().x, Cursor::Pos().y};
+		
+		if (update_map || (cursor_position.x != cursor_position_before.x && cursor_position.y != cursor_position_before.y)) {
+			cursor.coordinate = map.positionToCoordinate(cursor_position, camera);
+			cursor.position = map.coordinateToPosition(cursor.coordinate, camera);
+			
+			cursor_position_before = cursor_position;
+			update_map = true;
+		}
+		
+		//cout << cursor.coordinate.x << "," << cursor.coordinate.y << endl;
+		
+		// マップの描画
+		// マップを更新する必要がある場合はバッファに描画（更新）する
+		if (update_map) {
+			buffer_texture.clear(Color(50, 50, 50));
+			
+			ScopedRenderTarget2D target(buffer_texture);
+			map.draw(camera, cursor);
+			
+			if (first_loop) {
+				first_loop = false;
+				
+				// サブウィンドウの描画
+				/*
+				sub_window.getRenderTexture()->clear(Color(Palette::White));
+				ScopedRenderTarget2D target(*sub_window.getRenderTexture());
+				font16(U"テストですと").draw(0, 0, Color(Palette::Black));
+				sub_window.update();
+				
+				sub_window2.getRenderTexture()->clear(Color(Palette::White));
+				target = ScopedRenderTarget2D(*sub_window2.getRenderTexture());
+				font16(U"テストですよ").draw(0, 0, Color(Palette::Black));
+				sub_window2.update();*/
+			}
+			else {
+				update_map = false;
+			}
+		}
+		
+		// バッファを描画
+		buffer_texture.draw(0, 0);
+		//sub_window.draw();
+		//sub_window2.draw();
+		
+		System::Sleep(20);
+	}
+	
+	map.freeMapAndAddons();
+	
+	return;
+}

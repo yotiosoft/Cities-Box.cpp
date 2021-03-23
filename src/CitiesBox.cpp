2a70e1b7872a11743155c008e8efc8a464d17a68
yotiosoft <ytani0323@gmail.com>
2021-03-23 11:29:30 +0900

    CitiesBox.hppを作成

---
+++ src/CitiesBox.cpp
@@ -0,0 +1,8 @@
+//
+//  CitiesBox.cpp
+//  Cities-Box
+//
+//  Created by YotioSoft on 2021/03/23.
+//
+
+#include "CitiesBox.hpp"
---
+++ src/CitiesBox.hpp
@@ -0,0 +1,27 @@
+//
+//  CitiesBox.hpp
+//  Cities-Box
+//
+//  Created by YotioSoft on 2021/03/23.
+//
+
+#ifndef CitiesBox_hpp
+#define CitiesBox_hpp
+
+#include "AbstractSetting.hpp"
+#incldue "Addon.hpp"
+#include "AddonMaker.hpp"
+#include "Button.hpp"
+#include "CityMap.hpp"
+#include "DetailsBar.hpp"
+#include "GeneralSetting.hpp"
+#include "Images.hpp"
+#include "Menu.hpp"
+#include "PulldownMenu.hpp"
+#include "RouteSolver.hpp"
+#include "Sound.hpp"
+#include "StartUp.hpp"
+#include "SubWindow.hpp"
+#include "TitleMenu.hpp"
+
+#endif /* CitiesBox_hpp */

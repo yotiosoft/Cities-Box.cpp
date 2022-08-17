ddfe67c8c935d7a43974c202cb06922f5bf0b3c5
YotioSoft <ytani0323@gmail.com>
2022-08-17 21:31:23 +0900

    add debug log

--- src/ConnectableObject.cpp
+++ src/ConnectableObject.cpp
@@ -9,8 +9,11 @@
 
 void ConnectableObject::connect(CityNetwork& road_network, CoordinateStruct arg_connect_coordinate, Object *arg_object_p) {
 	// マップ上で接続
+	DirectionID::Type before = getDirectionID();
 	DirectionID::Type relative_direction_id = UnitaryTools::getDirectionIDfromDifference(m_start_coordinate + arg_connect_coordinate, arg_object_p->getOriginCoordinate(), !m_addon_p->isInCategories(CategoryID::Waterway));
 
+	UnitaryTools::debugLog(U"connect", arg_connect_coordinate, U"before: " + Format(before) + U" after: " + Format(relative_direction_id));
+	
 	// Directionが無効なら自身のオブジェクトを削除し終了
 	if (relative_direction_id == DirectionID::Disabled) {
 		UnitaryTools::debugLog(U"connect", arg_connect_coordinate, U"Direction disabled");
